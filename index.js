2d7627cb7c003ad5b359c9befeff22b99c61573d
Sam Detweiler <sdetweil@gmail.com>
2022-08-30 14:57:35 -0500

    mostly admin

--- index.js
+++ index.js
@@ -9,20 +9,29 @@ const Posts = require.main.require('./src/posts');
 
 
 const plugin = {};
+
 plugin.our_host =''
+
+// only use the literal in pne place
 const our_admin="/plugins/post_link_list"
+// get the key part
 const our_key = our_admin.split('/')[2]
+// default (test) hosts
 plugin.allowed_hosts=['github.com', 'pastebin.com','projects.raspberrypi.org', "forum.magicmirror.builders",'docs.magicmirror.builders']
 
+// init function
 plugin.init = function (params, callback) {
 
   console.log(our_admin +" entering init")
+
+  // go to the rener link
 	let renderAdminPage = function (req, res , next) {
-		console.log('admin'+our_admin +" renderAdminPage called")
+		console.log('admin'+our_admin +" renderAdminPage called, settings=",plugin.settings)
 		res.render('admin'+our_admin, {
-			our_admin: plugin.settings
+			post_link_list: plugin.settings
 		});
 	};
+
 	const router = params.router;
 	const hostMiddleware = params.middleware;
 	// const hostControllers = params.controllers;
@@ -33,23 +42,32 @@ plugin.init = function (params, callback) {
 	router.get('/admin'+our_admin, hostMiddleware.admin.buildHeader, renderAdminPage);
 	router.get('/api/admin'+our_admin, renderAdminPage);
 
+	// get the default settings
 	plugin.settings = plugin.allowed_hosts
 	// get our plugin  info , if set
 	meta.settings.get(our_key, function(err, settings) {
-		console.log(our_admin+" retrieve settings ", err, settings)
+		console.log(our_admin+" retrieved settings ", err, settings)
 		if (err) {
 			winston.error('['+our_key+'] Could not retrieve plugin settings!, using defaults');
 			//return;
 		}
 		else {
+			// hm, got here with no data.. ???
 			console.log(our_admin+" init found our settings ", settings)
 			//plugin.settings = settings;
 		}
 	});
-console.log(our_admin +" leaving init")
+	console.log(our_admin +" leaving init")
 	callback();
 };
 
+// watch dor data save
+plugin.actionSettingsSet = async function (hookData) {
+  if (hookData.plugin === our_key) {
+      plugin.settings = await meta.settings.get(our_key);
+  }
+};
+// add our admin page to the plugin menu in admin
 plugin.addAdminNavigation = function (header, callback) {
 	console.log("adding admin nav ")
 	header.plugins.push({
@@ -61,6 +79,7 @@ plugin.addAdminNavigation = function (header, callback) {
 	callback(null, header);
 };
 
+// add our data for the template to use
 plugin.onAdmin = function (data, callback) {
 			console.log(our_key+" onAdmin called")
 		data.templateValues[our_key] = plugin.settings;
@@ -68,7 +87,10 @@ plugin.onAdmin = function (data, callback) {
     callback(null, data);
 };
 
-
+// handler for post intending to go to moderation queue
+// we will check the links to see if they are appropriate for new users, spamers advertise their sites.
+// if an off site link is used, send to moderation queue, else let go thru without moderation,
+// don't imact real new users as much as possible
 plugin.postQueue = async function (postData) {
 	try {
 		// assume no links, or good links
@@ -77,6 +99,7 @@ plugin.postQueue = async function (postData) {
 		await Posts.parsePost(mockPost);
 		console.log(mockPost.content)
 		const regex = /href=\"([^"]*)"/ig;
+		// get just the href= for all the links
 		let links=mockPost.content.match(regex)
 		console.log("post data="+JSON.stringify(mockPost.content.match(regex),null,2))
 		postData.shouldQueue = false;
@@ -99,9 +122,10 @@ plugin.postQueue = async function (postData) {
 	return postData;
 };
 
+// check the link in our 'good' list
 plugin.checkLink = function(link) {
 
-
+	// if we haven't saved our url host yet,  do it now
 	if(plugin.our_host === ''){
 		// get our host, inlcuding port
 		plugin.our_host=nconf.get('url').toLowerCase().split('/')[2] //.split(':')[0]
@@ -124,12 +148,15 @@ plugin.checkLink = function(link) {
 	// get just the host from the link, including port
 	let link_host = link.toLowerCase().split('/')[2] //.split(':')[0]
 
+	// is there host link in our good list
 	if(plugin.allowed_hosts.includes(link_host)){
 		  console.log("allowed link to="+link_host)
+		  // yes
 			return true
 		}
 		else{
 			console.log("link to host "+ link_host+" not allowed")
+			// no
 			return false;
 		}
 
--- static/lib/admin.js
+++ static/lib/admin.js
@@ -3,9 +3,10 @@
 /* globals $, app, socket, define */
 const our_admina="/plugins/post_link_list"
 const our_keya = our_admina.split('/')[2]
-define('/admin'+ our_admina, ['settings'], function (Settings) {
-	var ACP = {};
 
+define('admin'+ our_admina, ['settings'], function (Settings) {
+	var ACP = {};
+		console.log(our_admina+ " admin define running")
 	ACP.init = function () {
 		console.log(our_keya+" loading our data")
 		Settings.load(our_keya, $('.'+our_keya+'_settings'));
